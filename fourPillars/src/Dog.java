3f9d58c0bd2b3b96880bde6073f0649a19451b6f
jrwatson94 <j.watsonreid@gmail.com>
2021-04-04 18:33:17 -0400

    initial commit

---
+++ fourPillars/src/Dog.java
@@ -0,0 +1,29 @@
+
+
+
+public class Dog {
+    private int energy = 10;
+    private int hunger = 10;
+
+    public static void main(String[] args) {
+        new Dog();
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+}
+
+
+
+
